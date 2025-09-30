8cda25e8d987020bda5ee60271ae829d673f4a30
Michaelninder <fabianeternis@gmail.com>
2025-09-30 17:25:48 +0200

    feat: Update scroll-to-top-btn with primary-color

--- resources/views/components/scroll-to-top-btn.blade.php
+++ resources/views/components/scroll-to-top-btn.blade.php
@@ -1,4 +1,4 @@
-<button id="scrollToTopBtn" class="fixed bottom-6 right-6 z-50 p-3 rounded-full bg-indigo-600 text-white shadow-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-indigo-700 dark:hover:bg-indigo-600 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-900 hidden transition-all duration-300">
+<button id="scrollToTopBtn" class="fixed bottom-6 right-6 z-50 p-3 rounded-full bg-primary-dark text-white shadow-lg hover:bg-primary focus:outline-none focus:ring-2 focus:ring-primary-light focus:ring-offset-2 dark:bg-primary-dark dark:hover:bg-primary dark:focus:ring-primary-light dark:focus:ring-offset-gray-900 hidden transition-all duration-300">
     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
     </svg>
