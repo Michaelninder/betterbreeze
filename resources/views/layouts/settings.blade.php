1067872aeeec94215b24082ca547cb6c42f78fa5
Michaelninder <fabianeternis@gmail.com>
2025-09-30 17:41:12 +0200

    feat: Update settings-side nav to be sticky and update profile.edit-view by adding margins ...

--- resources/views/layouts/settings.blade.php
+++ resources/views/layouts/settings.blade.php
@@ -5,7 +5,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
                 <div class="md:col-span-1">
-                    @include('components.settings-nav')
+                    <div class="sticky top-20">
+                        @include('components.settings-nav')
+                    </div>
                 </div>
                 <div class="md:col-span-3">
                     @yield('settings-content')
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.settings')
 
 @section('settings-content')
+<div class="space-y-6">
     <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
         <div class="max-w-xl">
             @include('profile.partials.update-profile-information-form')
@@ -18,4 +19,5 @@
             @include('profile.partials.delete-user-form')
         </div>
     </div>
+</div>
 @endsection
