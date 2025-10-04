082543c635c77ac45c8061ae5c4980865f1233da
Michaelninder <fabianeternis@gmail.com>
2025-10-04 19:32:29 +0200

    feat: Update components.theme-toggle

--- resources/views/components/theme-toggle.blade.php
+++ resources/views/components/theme-toggle.blade.php
@@ -37,38 +37,22 @@
     >
         <!-- System Icon -->
         <template x-if="theme === 'system'">
-            <svg xmlns="http://www.w3.org/2000/svg" 
-                 viewBox="0 0 24 24" 
-                 fill="none" stroke="currentColor" 
-                 stroke-width="1.5" 
-                 class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" 
-                      d="M9.75 17.25v1.25a2 2 0 01-.586 1.414L8.25 21h7.5l-.914-.914A2 2 0 0114.25 18.5v-1.25m5.5-12v11.25a1.75 1.75 0 01-1.75 1.75H6.75A1.75 1.75 0 015 15.25V5.25A1.75 1.75 0 016.75 3.5h10.5a1.75 1.75 0 011.75 1.75z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
             </svg>
         </template>
 
         <!-- Dark Icon -->
         <template x-if="theme === 'dark'">
-            <svg xmlns="http://www.w3.org/2000/svg" 
-                 viewBox="0 0 24 24" 
-                 fill="none" stroke="currentColor" 
-                 stroke-width="1.5" 
-                 class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" 
-                      d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
             </svg>
         </template>
-
+        
         <!-- Light Icon -->
         <template x-if="theme === 'light'">
-            <svg xmlns="http://www.w3.org/2000/svg" 
-                 viewBox="0 0 24 24" 
-                 fill="none" stroke="currentColor" 
-                 stroke-width="1.5" 
-                 class="w-6 h-6">
-                <circle cx="12" cy="12" r="5"/>
-                <path stroke-linecap="round" stroke-linejoin="round" 
-                      d="M12 1v2m0 18v2m11-11h-2M3 12H1m16.95 7.05l-1.41-1.41M6.46 6.46L5.05 5.05m12.9 0l-1.41 1.41M6.46 17.54l-1.41 1.41"/>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
             </svg>
         </template>
     </button>
