b577bc413c3d8ce31c29d95698c82040499106d1
Michaelninder <fabianeternis@gmail.com>
2025-10-27 20:11:30 +0100

    END OF DAY (MAYBE)

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -18,14 +18,14 @@
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <div class="ms-3 relative">
+                <div class="ms-0 relative">
                     <x-locale-switcher />
                 </div>
-                <div class="ms-3 relative">
+                <div class="ms-0 relative">
                     <x-theme-toggle />
                 </div>
                 @auth
-                <div class="ms-3 relative ml-2">
+                <div class="ms-1 relative ml-2">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
